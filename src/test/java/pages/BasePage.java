c39456c94840fcd5bf3eaaa36f187c071c25cb04
kevin Mendez <kmendezp@unal.edu.co>
2018-08-29 11:04:14 -0500

    fail

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
     protected WebDriver driver;
-    public BasePage(WebDriver driver){
+
+    public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
